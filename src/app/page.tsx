0b2f3aa801d02665615395da925400f5545717d9
Nick Echols <stere.ereo@gmail.com>
2023-12-23 21:59:27 -0800

    feat: more info on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,25 @@ import styles from "./page.module.css";
 import { getStatus } from "@/lib/getStatus";
 
 function getRecentStatuses() {
-  return sql`SELECT * FROM statuses ORDER BY "created_at" DESC`;
+  return sql`SELECT * FROM statuses ORDER BY created_at DESC LIMIT 10`;
 }
 
 function getCount() {
   return sql`SELECT COUNT(*) FROM statuses`;
 }
 
+function getFirstStatusDate() {
+  return sql`SELECT created_at FROM statuses ORDER BY created_at ASC LIMIT 1`;
+}
+
 export default async function Home() {
-  const [currentStatus, recentStatuses, count] = await Promise.all([
-    getStatus(),
-    getRecentStatuses(),
-    getCount(),
-  ]);
+  const [currentStatus, recentStatuses, count, firstStatusDate] =
+    await Promise.all([
+      getStatus(),
+      getRecentStatuses(),
+      getCount(),
+      getFirstStatusDate(),
+    ]);
 
   if (typeof currentStatus === "undefined") return <span>error</span>;
 
@@ -24,16 +30,32 @@ export default async function Home() {
     <main className={styles.main}>
       <h2>
         The Base to Base is currently{" "}
-        <span color={currentStatus.statusColour}>{currentStatus.status}</span>
+        <span color={currentStatus.statusColour}>
+          ✨{currentStatus.status}✨
+        </span>
       </h2>
       <section>
-        <h3>
-          {`We have currently collected ${count.rows[0].count} Base to Base statuses. We will use these to figure out how much of the scheduled time the Base to Base is down`}
-        </h3>
-        <p>Here are some recent statuses:</p>
-        <pre>
-          <code>{JSON.stringify(recentStatuses.rows, null, 2)}</code>
-        </pre>
+        <p>
+          {`We have currently collected ${
+            count.rows[0].count
+          } Base to Base statuses (every 5 minutes since ${new Date(
+            firstStatusDate.rows[0].created_at
+          ).toLocaleString()}).`}
+        </p>
+        <p>
+          We will use these to figure out how much of the scheduled time the
+          Base to Base is down
+        </p>
+      </section>
+      <section>
+        <h3>Here are some recent statuses</h3>
+        <ul>
+          {recentStatuses.rows.map(({ id, status, created_at }) => (
+            <li key={id}>
+              {`${status} ${new Date(created_at).toLocaleString()}`}
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
