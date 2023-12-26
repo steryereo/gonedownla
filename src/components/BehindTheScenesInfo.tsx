7792f84b9c89b7f9e43cd702d4265f2629a5d362
Nick Echols <stere.ereo@gmail.com>
2023-12-25 23:26:32 -0800

    feat: hide behind the scenes info by default

--- src/components/BehindTheScenesInfo.tsx
+++ src/components/BehindTheScenesInfo.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import type { QueryResultRow } from "@vercel/postgres";
+import { useState } from "react";
+
+import styles from "../app/page.module.css";
 
 export default function BehindTheScenesInfo({
   count,
@@ -15,28 +18,37 @@ export default function BehindTheScenesInfo({
   openCount: number;
   withinHoursCount: number;
 }) {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <>
-      <section>
-        <p>
-          {" "}
-          collected {count} Base to Base statuses (every 5 minutes since{" "}
-          {new Date(firstStatusDate).toLocaleString()}). {withinHoursCount}{" "}
-          within normal operating hours. For {openCount} of those, the Base to
-          Base has been open
-        </p>
-      </section>
-      <section>
-        <h3>Here are some recent statuses</h3>
-        <ul>
-          {statuses.map(({ id, status, created_at }) => (
-            <li key={id}>
-              <strong>{status}</strong>
-              {` ${new Date(created_at).toLocaleString()}`}
-            </li>
-          ))}
-        </ul>
-      </section>
+      {/* TODO: a11y text for button*/}
+      <button className={styles.button} onClick={() => setShowInfo(!showInfo)}>
+        ⓘ
+      </button>
+      {showInfo ? (
+        <>
+          <section>
+            <p>
+              collected {count} Base to Base statuses (every 5 minutes since{" "}
+              {new Date(firstStatusDate).toLocaleString()}). {withinHoursCount}{" "}
+              within normal operating hours. For {openCount} of those, the Base
+              to Base has been open
+            </p>
+          </section>
+          <section>
+            <h3>Here are some recent statuses</h3>
+            <ul>
+              {statuses.map(({ id, status, created_at }) => (
+                <li key={id}>
+                  <strong>{status}</strong>
+                  {` ${new Date(created_at).toLocaleString()}`}
+                </li>
+              ))}
+            </ul>
+          </section>
+        </>
+      ) : null}
     </>
   );
 }
